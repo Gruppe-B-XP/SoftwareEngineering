a28841300da93198570943426c5729d8d95c2312
Dario Viva <45972949+DarioViva42@users.noreply.github.com>
2020-04-12 12:25:00 +0200

    teleporting

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using UnityEngine;
+using Valve.VR;
+
+public class Teleporter : MonoBehaviour
+{
+    public GameObject m_Pointer;
+    public SteamVR_Action_Boolean m_TeleportAction = null;
+
+    private SteamVR_Behaviour_Pose m_Pose = null;
+    private bool m_HasPosition = false;
+    private bool m_IsTeleporting = false;
+    private float m_FadeTime = 0.5f;
+
+    private void Awake()
+    {
+        m_Pose = GetComponent<SteamVR_Behaviour_Pose>();
+    }
+
+    private void Update()
+    {
+        // Pointer
+        m_HasPosition = UpdatePointer();
+        m_Pointer.SetActive(m_HasPosition);
+        
+        // Teleport
+        if (m_TeleportAction.GetStateUp(m_Pose.inputSource))
+        {
+            print("Teleport pressed");
+            TryTeleport();
+        }
+
+    }
+
+    private void TryTeleport()
+    {
+        // Check for valid position, and if already teleporting
+        if (!m_HasPosition || m_IsTeleporting)
+            return;
+
+        print(SteamVR_Render.Top().origin);
+        // Get camera rig, and head position
+        Transform cameraRig = SteamVR_Render.Top().origin;
+        Vector3 headPosition = SteamVR_Render.Top().head.position;
+
+        // Figure out translation
+        Vector3 groundPosition = new Vector3(headPosition.x, cameraRig.position.y, headPosition.z);
+        Vector3 translateVector = m_Pointer.transform.position - groundPosition;
+
+        // Move
+        StartCoroutine(MoveRig(cameraRig, translateVector));
+    }
+
+    private IEnumerator MoveRig(Transform cameraRig, Vector3 tranlation)
+    {
+        // Flag
+        m_IsTeleporting = true;
+
+        // Fade to black
+        SteamVR_Fade.Start(Color.black, m_FadeTime, true);
+
+        // Apply translation
+        yield return new WaitForSeconds(m_FadeTime);
+        cameraRig.position += tranlation;
+
+        // Fade to clear
+        SteamVR_Fade.Start(Color.clear, m_FadeTime, true);
+
+        // De-flag
+        yield return new WaitForSeconds(m_FadeTime);
+        m_IsTeleporting = false;
+    }
+
+    private bool UpdatePointer()
+    {
+        // Ray from the controller
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        // If it's a hit
+        if(Physics.Raycast(ray, out hit))
+        {
+            m_Pointer.transform.position = hit.point;
+            return true;
+        }
+
+        // If not a hit
+        return false;
+    }
+}
