0aacbf3cfda2876eef84db1498b4389078a1e2be
Luis Arnold <45848951+Bakanagoku@users.noreply.github.com>
2020-04-12 12:22:12 +0200

    Locomotion

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 TouchpadPosition
+        {
+            get
+            {
+                return SteamVR_Actions.default_TouchpadPosition;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
