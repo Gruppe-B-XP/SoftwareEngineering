68658c65684b800266ca468f78dfaf0bc26b8ace
murersDHBW <37251616+silmurer@users.noreply.github.com>
2020-05-23 19:15:10 +0200

    changeScene implementiert

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    //[SerializeField] private string model;
+    public void ButtonMoveScene(string model)
+    {
+        SceneManager.LoadScene(model);
+    }
+}
