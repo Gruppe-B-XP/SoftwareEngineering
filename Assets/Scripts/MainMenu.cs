8b20573aae7f1daf1f595d32f366059270b3ae54
Niklas <46811902+2Who@users.noreply.github.com>
2020-05-25 22:47:37 +0200

    Added path selection

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Networking.Types;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
 
+    public string modelPath;
+
     public void StartProgram()
     {
         SceneManager.LoadSceneAsync("Galerie");
+    }
+
+    public void QuitProgram()
+    {
+        Application.Quit();
+    }
+
+    public void SelectModel()
+    {
+        modelPath = EditorUtility.OpenFilePanel("Ein Modell auswählen", "", "");
+        Debug.Log(modelPath);
     }
 
 }
