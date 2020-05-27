cdfe4515bc238ac735274db90efc14b8e5c2b6ed
murersDHBW <37251616+silmurer@users.noreply.github.com>
2020-05-27 22:21:06 +0200

    Plazieren automatisiert

---
+++ Assets/Scripts/PlaceModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceModel : MonoBehaviour
+{
+        public Transform _theAsset;
+        public Vector3 _spawnPosition;
+
+        //Call this.Spawn() from Start() or Update() or whereever your logic triggers the spawn of the asset
+        void Start()
+        {
+            Transform instanciatedAsset;
+
+            instanciatedAsset = (Transform)Instantiate(this._theAsset, 0,0,0, Quaternion.identity);
+            //Do stuff with your instanciatedAsset ...
+        }
+    }
