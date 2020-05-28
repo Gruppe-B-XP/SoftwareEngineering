076a9d01a887be18327b7b49d093e2cb7ab19350
Niklas <46811902+2Who@users.noreply.github.com>
2020-05-28 11:59:53 +0200

    Mainmenu Script erneut hinzugefügt

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -19,10 +19,7 @@ public class MainMenu : MonoBehaviour
 
     public void SelectModel()
     {
-        // Folgendes kann nicht funktionieren. Macht das bitte anders.
-        // Siehe https://stackoverflow.com/questions/56813826/unity-build-error-the-name-editorutility-does-not-exist-in-the-current-contex
-        // Vielen Dank
-        // modelPath = EditorUtility.OpenFilePanel("Ein Modell auswählen", "", "");
+        modelPath = EditorUtility.OpenFilePanel("Ein Modell auswählen", "", "");
         Debug.Log(modelPath);
     }
 
