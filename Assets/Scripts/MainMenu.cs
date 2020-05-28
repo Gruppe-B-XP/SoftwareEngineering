820e079b0c12c8abe17db1f9b247675c62c81707
murersDHBW <37251616+silmurer@users.noreply.github.com>
2020-05-28 15:05:14 +0200

    Load .obj

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,13 +2,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Dummiesman;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MainMenu : MonoBehaviour
 {
 
     public string modelPath;
     public GameObject myModel;
-    public Scene sceneToLoad = SceneManager.GetSceneByName("Galerie");
+    //public Scene sceneToLoad = SceneManager.GetSceneByName("Galerie");
+    public Vector3 spawnPosition;
 
     public void StartProgram()
     {
@@ -27,6 +30,7 @@ public class MainMenu : MonoBehaviour
         modelPath = EditorUtility.OpenFilePanel("Ein Modell auswählen", "", "obj");
         myModel = new OBJLoader().Load(modelPath);
         Debug.Log(myModel.name);
+        Instantiate(myModel, this.spawnPosition, Quaternion.identity);
     }
 
 }
--- Assets/Scripts/changeScene.cs
+++ Assets/Scripts/changeScene.cs
@@ -2,12 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using Dummiesman;
+using System.Collections.Specialized;
 
 public class ChangeScene : MonoBehaviour
 {
-    public string NewScene;
+    //public string NewScene;
+    public string modelPath;
+    public GameObject myModel;
+    public Vector3 spawnPosition, scaleChange;
+    public GameObject player;
+
     void OnMouseDown()
     {
-        SceneManager.LoadScene(NewScene);
+
+        //Selecting and importing a file
+
+
+        modelPath = EditorUtility.OpenFilePanel("Ein Modell auswählen", "", "obj");
+        myModel = new OBJLoader().Load(modelPath);
+        Debug.Log(myModel.name);
+        //Instantiate(myModel, this.spawnPosition, Quaternion.identity);
+        // new Vector3 scaleChange2 = new Vector3(-0.001f, -0.001f, -0.001f);
+        myModel.transform.localScale = scaleChange;
+        player.transform.position = new Vector3(0, 0, 75);
+
+
+
     }
 }
