c2ca96ff9ecbd76c2fe5cf9d3e065021beb753d3
Niklas <46811902+2Who@users.noreply.github.com>
2020-05-29 20:29:26 +0200

    Deleted double line

--- Assets/Scripts/Import.cs
+++ Assets/Scripts/Import.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.SceneManagement;
 using Dummiesman;
 using System.Collections.Specialized;
 using SimpleFileBrowser;
