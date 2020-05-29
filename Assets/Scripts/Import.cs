1d2fac4752e68be571c5881f4975135690ca0a00
murersDHBW <37251616+silmurer@users.noreply.github.com>
2020-05-29 12:30:58 +0200

    Fixing some fixables

--- Assets/Scripts/Import.cs
+++ Assets/Scripts/Import.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -11,19 +12,6 @@ using System.Diagnostics;
 
 public class Import : MonoBehaviour
 {
-    using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.SceneManagement;
-using Dummiesman;
-using System.Collections.Specialized;
-using SimpleFileBrowser;
-using System.Runtime.InteropServices;
-using System.Diagnostics;
-
-public class ChangeScene : MonoBehaviour
-{
     public string modelPath;
     public GameObject myModel;
     public Vector3 spawnPosition, scaleChange;
@@ -34,17 +22,13 @@ public class ChangeScene : MonoBehaviour
         //Selecting and importing a file
         FileBrowser.SetFilters(true, ".obj");
         FileBrowser.SetDefaultFilter(".obj");
-        FileBrowser.ShowLoadDialog((path) => { modelPath = path; }, () => { }, false, null, "Select Model", "Select");
-        while (String.IsNullOrEmpty(modelPath) == true)
-        {
-            print("wait");
-        }
+        FileBrowser.ShowLoadDialog((path) => { modelPath = path; }, null, false, null, "Select Model", "Select");
+        //while (String.IsNullOrEmpty(modelPath) == true) { }
         //yield return new WaitUntil(() => String.IsNullOrEmpty(modelPath) == false);
 
         myModel = new OBJLoader().Load(modelPath);
         //myModel = new OBJLoader().Load("C:/Users/silas/Desktop/Neumatt-3D.obj");
         myModel.transform.localScale = scaleChange;
-        //myModel.GetComponentInChildren<Renderer>().material = "blue" ;
         player.transform.position = new Vector3(0, 0, 10);
 
 
@@ -53,5 +37,4 @@ public class ChangeScene : MonoBehaviour
 
 }
 
-}
 
